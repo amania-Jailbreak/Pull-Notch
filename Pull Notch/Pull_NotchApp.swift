dd67817774389822a2d0cee1dd3521480ba724c7
amania-jailbreak <amania.apple@icloud.com>
2026-03-30 07:53:48 +0900

    Initial Commit

---
+++ Pull Notch/Pull_NotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pull_NotchApp.swift
+//  Pull Notch
+//
+//  Created by amania on 2026/03/30.
+//
+
+import SwiftUI
+
+@main
+struct Pull_NotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
